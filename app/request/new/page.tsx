b2ac6500886db03db3a4ca5ac465f27429606212
alisiahi <alisiahi.1991@gmail.com>
2024-08-22 17:33:36 +0200

    redirecting non-authenticated user from the create new request page

--- app/request/new/page.tsx
+++ app/request/new/page.tsx
@@ -1,7 +1,14 @@
 import { InspectionRequestForm } from "@/components/forms/RequestForm";
-import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const NewRequest = () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   return (
     <div className="flex flex-col min-h-screen justify-center items-center">
       <InspectionRequestForm mode="create" />
